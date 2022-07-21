4ec98236d870771e0beed3793984c8a9a64704e1
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2022-07-21 15:20:08 -0400

    fix: Formated file

--- Reporte1.sql
+++ Reporte1.sql
@@ -1,38 +1,28 @@
 CREATE OR REPLACE PROCEDURE REPORTE_12 (prc out sys_refcursor,fecha_inicio date,tipo_cliente VARCHAR2(100))
-IS 
+IS
 BEGIN
     IF ((fechainicio != 'null')) THEN
 
-        OPEN PRC FOR SELECT r.fechas.fecha_inicial  "MES",c.tipo "TIPO_CLIENTE",r.calificacion.escala "ESCALA_ClASIFICACION" ,r.calificacion.observaciones"OBSERVACIONES" 
+        OPEN PRC FOR SELECT r.fechas.fecha_inicial  "MES",c.tipo "TIPO_CLIENTE",r.calificacion.escala "ESCALA_ClASIFICACION" ,r.calificacion.observaciones"OBSERVACIONES"
         FROM RESERVA R
-        INNER JOIN CLIENTE C    
-        where  r.cliente_id   = c.id 
+        INNER JOIN CLIENTE C
+        where  r.cliente_id   = c.id
         and    tipo_cliente = c.tipo
         and to_char(fecha_inicio,'mm') = to_char(r.fechas.fecha_inicial,'mm') and to_char(fecha_inicio,'yy') = to_char(r.fechas.fecha_inicial,'yy')
-        
-
-
     END IF;
 END;
 
 CREATE OR REPLACE PROCEDURE REPORTE_10 (prc out sys_refcursor,fecha_inicio date,tipo_cliente VARCHAR2(100))
-IS 
+IS
 BEGIN
     IF ((fechainicio != 'null')) THEN
-
-        OPEN PRC FOR SELECT p.fechas.fecha_inicial  "MES",p.tipo_cliente "TIPO_CLIENTE",p.nombre "NOMBRE_PLAN" ,COUNT(p.id) "PLANES_CONTRATADOS" 
+        OPEN PRC FOR SELECT p.fechas.fecha_inicial  "MES",p.tipo_cliente "TIPO_CLIENTE",p.nombre "NOMBRE_PLAN" ,COUNT(p.id) "PLANES_CONTRATADOS"
         FROM PLANES P
-        INNER JOIN CLIENTE C    
+        INNER JOIN CLIENTE C
         where tipo_cliente = p.tipo_cliente
         and to_char(fecha_inicio,'mm') = to_char(p.fechas.fecha_inicial,'mm') and to_char(fecha_inicio,'yy') = to_char(p.fechas.fecha_inicial,'yy')
-        
-
-
     END IF;
 END;
-
-
-
 
 set autoprint on;
 VARIABLE MEMORYCURSOR REFCURSOR;
