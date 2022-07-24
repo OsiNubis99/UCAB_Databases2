2af039c1bc340505e22fb994e615e85fff01e869
alexis <sixela.quiros@gmail.com>
2022-07-24 12:54:46 -0400

    reporte10 acomodado

--- Reporte10.sql
+++ Reporte10.sql
@@ -1,26 +1,15 @@
-CREATE OR REPLACE PROCEDURE REPORTE_12 (prc out sys_refcursor,fecha_inicio date,tipo_cliente VARCHAR2(100))
-IS
-BEGIN
-    IF ((fechainicio != 'null')) THEN
-
-        OPEN PRC FOR SELECT r.fechas.fecha_inicial  "MES",c.tipo "TIPO_CLIENTE",r.calificacion.escala "ESCALA_ClASIFICACION" ,r.calificacion.observaciones"OBSERVACIONES"
-        FROM RESERVA R
-        INNER JOIN CLIENTE C
-        where  r.cliente_id   = c.id
-        and    tipo_cliente = c.tipo
-        and to_char(fecha_inicio,'mm') = to_char(r.fechas.fecha_inicial,'mm') and to_char(fecha_inicio,'yy') = to_char(r.fechas.fecha_inicial,'yy')
-    END IF;
-END;
 
 CREATE OR  REPLACE FUNCTION SYSTEM.REPORTE10 (prc out sys_refcursor,fecha_inicio date,tipo_cliente VARCHAR2(100))
 IS
 BEGIN
-    IF ((fechainicio != 'null')) THEN
-        OPEN PRC FOR SELECT p.fechas.fecha_inicial  "MES",p.tipo_cliente "TIPO_CLIENTE",p.nombre "NOMBRE_PLAN" ,COUNT(p.id) "PLANES_CONTRATADOS"
+    IF ((fechainicio != 'null')) THEN""
+        OPEN PRC FOR SELECT p.fechas.fecha_inicial  "MES",h.foto "CATEGORIA_PLAN",p.tipo_cliente "TIPO_CLIENTE",p.nombre "NOMBRE_PLAN" ,COUNT(p.id) "PLANES_CONTRATADOS"
         FROM PLANES P
         INNER JOIN CLIENTE C
-        where tipo_cliente = p.tipo_cliente
-        and to_char(fecha_inicio,'mm') = to_char(p.fechas.fecha_inicial,'mm') and to_char(fecha_inicio,'yy') = to_char(p.fechas.fecha_inicial,'yy')
+        on tipo_cliente = p.tipo_cliente
+        INNER JOIN CATEGORITA H
+        on h.id = p.categoria_id
+        WHERE to_char(fecha_inicio,'mm') = to_char(p.fechas.fecha_inicial,'mm') and to_char(fecha_inicio,'yy') = to_char(p.fechas.fecha_inicial,'yy')
     END IF;
 END;
 
